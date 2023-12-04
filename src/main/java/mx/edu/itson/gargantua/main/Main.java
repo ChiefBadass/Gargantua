0b1e984ff5986a2c9d669fab3c84494ddab5381d
ArianGastelum <114516965+ArianGastelum@users.noreply.github.com>
2023-12-03 22:10:04 -0700

    Documentación de código

--- src/main/java/mx/edu/itson/gargantua/main/Main.java
+++ src/main/java/mx/edu/itson/gargantua/main/Main.java
@@ -1,6 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Clase Main - Punto de entrada principal de la aplicación Gargantua.
+ *
+ * En este punto de entrada, se realiza una serie de operaciones utilizando las clases y patrones definidos:
+ * - Se obtiene una instancia única de la clase Conexion, aplicando el patrón Singleton.
+ * - Se utiliza el patrón Factory en la clase CreacionTarea para crear instancias de la clase Tarea y agregarlas a la lista de tareas.
+ * - Se utiliza el patrón Adapter en la clase AdapterTarea para mostrar las descripciones de las tareas almacenadas en la lista.
+ * - Se elimina una tarea de la lista para demostrar la manipulación de la lista de tareas.
+ * - Se vuelve a utilizar el Adapter para mostrar las tareas restantes en la lista.
+ *
+ * La aplicación ilustra el uso coordinado de las clases y patrones mencionados para la gestión de tareas.
+ *
+ * 
  */
 package mx.edu.itson.gargantua.main;
 
@@ -17,6 +27,7 @@ import mx.edu.itson.gargantua.entidades.Tarea;
 public class Main {
     
     public static void main(String[] args) {
+         // Obtener una instancia única de la clase Conexion utilizando el patrón Singleton
         Conexion listaTareas = Conexion.obtenerInstancia();
 
         // Utilizando el Factory para crear tareas
